deadfbc9484a96d113a7fa2e8d58643489c1289d
invakid404 <invakid404@riseup.net>
2024-05-24 21:07:31 +0300

    feat: generate oauth client crd

--- cmd/gencrds/main.go
+++ cmd/gencrds/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
 	"fmt"
 	"github.com/invopop/jsonschema"
 	"github.com/ory/fosite"
 	"github.com/stoewer/go-strcase"
+	"log"
+	"text/template"
 )
 
+//go:embed oauthClient.tmpl
+var oauthConfigTemplate string
+
 func main() {
+	tmpl, err := template.New("oauthClient").Parse(oauthConfigTemplate)
+	if err != nil {
+		log.Fatalln("failed to parse template:", err)
+	}
+
 	reflector := new(jsonschema.Reflector)
 	reflector.AllowAdditionalProperties = true
 	reflector.KeyNamer = strcase.LowerCamelCase
@@ -16,5 +28,13 @@ func main() {
 	spec := schema.Definitions["DefaultClient"]
 
 	data, _ := spec.MarshalJSON()
-	fmt.Println(string(data))
+
+	var output bytes.Buffer
+	if err = tmpl.Execute(&output, map[string]any{
+		"Spec": string(data),
+	}); err != nil {
+		log.Fatalln("failed to render template:", err)
+	}
+
+	fmt.Println(string(output.Bytes()))
 }
